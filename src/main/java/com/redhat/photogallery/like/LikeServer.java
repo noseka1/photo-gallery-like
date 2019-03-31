d63af762fbf55bbe541268a3aab98df08d475c6c
Ales Nosek <anosek@redhat.com>
2019-03-30 23:43:22 -0700

    Use common Vert.x init

--- src/main/java/com/redhat/photogallery/like/LikeServer.java
+++ src/main/java/com/redhat/photogallery/like/LikeServer.java
@@ -1,17 +1,15 @@
 package com.redhat.photogallery.like;
 
 import com.redhat.photogallery.common.Server;
-
-import io.vertx.core.logging.SLF4JLogDelegateFactory;
-import io.vertx.reactivex.core.Vertx;
+import com.redhat.photogallery.common.VertxInit;
 
 public class LikeServer {
 
 	private static final int LISTEN_PORT = 8081;
 
 	public static void main(String[] args) {
-		System.setProperty("vertx.logger-delegate-factory-class-name", SLF4JLogDelegateFactory.class.getName());
-		Vertx vertx = Vertx.vertx();
-		vertx.deployVerticle(new Server(LISTEN_PORT, new LikeComponent()));
+		VertxInit.createClusteredVertx(vertx -> {
+			vertx.deployVerticle(new Server(LISTEN_PORT, new LikeComponent()));
+		});
 	}
 }
